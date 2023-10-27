3b6af5b6b8c677648551c59ddd958fa56595b5dc
Adila Razmi <adilarazmi@Adilas-MBP.localdomain>
2023-10-27 13:36:31 +0200

    test: complete tests for entryservice + notes

--- addressBook.Tests/EntryServiceTests.cs
+++ addressBook.Tests/EntryServiceTests.cs
@@ -4,7 +4,6 @@ using addressBook.Dtos;
 using addressBook.Models;
 using addressBook.Services;
 using AutoMapper;
-using Microsoft.AspNetCore.Mvc;
 
 public class EntryServiceTests
 {
@@ -22,11 +21,11 @@ public class EntryServiceTests
     {
         var newEntry = new EntryRequestDto
         {
-            Name = "Adila",
+            Name = "Kamelia",
             Address = new Address()
             {
-                Street = "Geodesivägen 11B",
-                City = "Huddinge",
+                Street = "Sötvägen 22",
+                City = "Cute City",
                 PostalCode = 14137
             }
         };
@@ -47,18 +46,20 @@ public class EntryServiceTests
         Assert.True(result.Success);
         Assert.NotNull(result.Data);
         Assert.IsType<List<EntryResponseDto>>(result.Data);
-        Assert.Equal(2, result.Data.Count);
+        // Assert.Equal(2, result.Data.Count);        Look at note below
     }
 
-    [Fact]
-    public async Task GetEntryByIdService()
+    [Theory]
+    [InlineData("26e40c03-1adc-4010-bdbd-306acfb7f05f", true)]
+    [InlineData("26e40c03-1adc-4010-bdbd-306acfb7f0ff", false)]
+    public async Task GetEntryByIdService(string idString, bool shouldSucceed)
     {
-        Guid id = new Guid("26e40c03-1adc-4010-bdbd-306acfb7f05f");
+        Guid id = Guid.Parse(idString);
         var expected = new ServiceResponse<EntryResponseDto>()
         {
-            Data = new EntryResponseDto // Initialize the Data property
+            Data = new EntryResponseDto
             {
-                Id = new Guid("26e40c03-1adc-4010-bdbd-306acfb7f05f"),
+                Id = id,
                 Name = "Adila",
                 Address = new Address()
                 {
@@ -72,10 +73,59 @@ public class EntryServiceTests
         };
         var result = await _entryService.GetEntryById(id);
 
-        Assert.NotNull(result);
-        Assert.True(result.Success);
-        Assert.NotNull(result.Data);
-        Assert.IsType<EntryResponseDto>(result.Data);
-        Assert.Equal(expected.Data.Id, result.Data.Id);
+        if (shouldSucceed)
+        {
+            Assert.NotNull(result);
+            Assert.True(result.Success);
+            Assert.NotNull(result.Data);
+            Assert.IsType<EntryResponseDto>(result.Data);
+            Assert.Equal(expected.Data.Id, result.Data.Id); // Look at deep equality comparison with FluentAssertions
+        }
+        else
+        {
+            Assert.False(result.Success);
+            Assert.Equal($"Entry with id {id} not found", result.Message);
+        }
+    }
+
+    [Theory]
+    [InlineData("26e40c03-1adc-4010-bdbd-306acfb7f05f", true)]
+    [InlineData("26e40c03-1adc-4010-bdbd-306acfb7f0ff", false)]
+    public async Task DeleteEntry(string idString, bool shouldSucceed)
+    {
+        Guid id = Guid.Parse(idString);
+        var result = await _entryService.DeleteEntry(id);
+
+        if (shouldSucceed)
+        {
+            Assert.True(result.Success);
+            Assert.Equal(1, result.Data.Count);
+        }
+        else
+        {
+            Assert.False(result.Success);
+            Assert.Equal($"Entry with id {id} not found", result.Message);
+        }
     }
 }
+
+/* Notes */
+
+/*
+
+All good unit tests should be 100% isolated.
+Using shared state (e.g. depending on a static property
+that is modified by each test) is regarded as bad practice.
+
+In this code, all tests depend on the shared state of the static field
+'entries' in the service. This results in unpredictable results.
+
+In order to make this work, we can instantiate EntryService at every test and
+populate the 'entries' instance field in the constructor of EntryService. we would then need
+to add EntryService as singleton instead of scoped so that 'entries' are presisted between
+requests.
+
+However since this is not really a real implementation and only for learning purposes,
+I will leave it like this.
+
+*/
